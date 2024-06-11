b16249b436fc5bdaeba2e79f1f1600d27e71b01a
X4nn4 <114231575+X4nn4@users.noreply.github.com>
2024-06-11 12:23:25 +0200

    Add files via upload

--- XannaShell.py
+++ XannaShell.py
@@ -1,17 +1,85 @@
 from tkinter import *
 import tkinter as tk
 import os
-import platform as pl
 from tkinter import scrolledtext
 from tkinter import Menu
 from io import open
 from tkinter import PanedWindow
 from tkinter import ttk
-from tkinter import messagebox
-import time
 import psutil
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtCore import QTimer
+from pyqtgraph import PlotWidget
+import sys
 
 #--------FUNCTIONS AND CLASS---------#
+class MonitorRendimiento(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Monitor de Rendimiento')
+
+        # Configurar el layout principal
+        layout = QVBoxLayout()
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.central_widget.setLayout(layout)
+
+        # Configurar el gráfico de líneas para la CPU
+        self.cpu_plot = PlotWidget()
+        layout.addWidget(self.cpu_plot)
+        self.cpu_plot.setYRange(0, 100)
+        self.cpu_plot.setLabel('left', 'Uso de CPU (%)')
+        self.cpu_plot.setLabel('bottom', 'Tiempo (s)')
+        self.cpu_curve = self.cpu_plot.plot(pen='r')
+
+        # Configurar el gráfico de líneas para la memoria
+        self.memory_plot = PlotWidget()
+        layout.addWidget(self.memory_plot)
+        self.memory_plot.setYRange(0, 100)
+        self.memory_plot.setLabel('left', 'Uso de Memoria (%)')
+        self.memory_plot.setLabel('bottom', 'Tiempo (s)')
+        self.memory_curve = self.memory_plot.plot(pen='b')
+
+        # Configurar el gráfico de líneas para el disco
+        self.disk_plot = PlotWidget()
+        layout.addWidget(self.disk_plot)
+        self.disk_plot.setYRange(0, 100)
+        self.disk_plot.setLabel('left', 'Uso de Disco (%)')
+        self.disk_plot.setLabel('bottom', 'Tiempo (s)')
+        self.disk_curve = self.disk_plot.plot(pen='g')
+
+        # Inicializar los datos y el temporizador
+        self.cpu_history = []
+        self.memory_history = []
+        self.disk_history = []
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start(1000)
+
+    def update_plot(self):
+        # Obtener el uso de la CPU y la memoria
+        cpu_usage = psutil.cpu_percent()
+        memory_usage = psutil.virtual_memory().percent
+
+        # Obtener el uso del disco
+        disk_usage = psutil.disk_usage('/').percent
+
+        # Agregar datos al historial
+        self.cpu_history.append(cpu_usage)
+        self.memory_history.append(memory_usage)
+        self.disk_history.append(disk_usage)
+
+        # Actualizar los datos en los gráficos de líneas
+        self.cpu_curve.setData(self.cpu_history)
+        self.memory_curve.setData(self.memory_history)
+        self.disk_curve.setData(self.disk_history)
+
+def rendimiento():
+    app = QApplication(sys.argv)
+    window = MonitorRendimiento()
+    window.setGeometry(100, 100, 800, 600)
+    window.show()
+    app.exec_()
 
 def ocultar_paneles():
     pw_red.pack_forget()
@@ -24,6 +92,9 @@ def ocultar_paneles():
     pw_filtrado.pack_forget()
     pw_servicios.pack_forget()
     pw_administrar_grupos.pack_forget()
+    pw_formateo_realizado.pack_forget()
+    pw_eventos.pack_forget()
+    pw_eventos_activos.pack_forget()
 
 def get_storage_devices():
     # Obtener todos los dispositivos de almacenamiento disponibles en el sistema
@@ -35,10 +106,22 @@ def get_storage_devices():
             devices.append(device)
     return devices
 
+def get_event_type():
+    lista = ["Application","Active Directory Web Services","DFS Replication","Directory Service","DNS Server","HardwareEvents","Internet Explorer","Key Management Service","Security","System","Windows Powershell"]
+    return lista
+
 def formateo():
-    seleccion=str(combo_dispositivos.get())
-    os.system(f"powershell.exe -command 'Format-Volume -DriveLetter {seleccion} -Confirm:$false'")
-    print("Formateo realizado")
+    ocultar_paneles()
+    pw_formateo_realizado.pack()
+    seleccion=str(combo_dispositivos.get()).replace(':', ' ')
+    modificada=seleccion.replace('\\', ' ')
+    t12.delete("1.0",END)
+    t12.update()
+    t12.insert(END, "Loading...")
+    t12.update()
+    info = os.popen(f'powershell.exe -command "Format-Volume -DriveLetter {modificada}"').read()
+    t12.delete("1.0",END)
+    t12.insert(1.0, info)
 
 def formatear():
     ocultar_paneles()
@@ -115,6 +198,44 @@ def servicios():
     t9.delete("1.0",END)
     t9.insert(1.0, info)
 
+def eventlog():
+    ocultar_paneles()
+    pw_eventos.pack()
+    t13.delete("1.0",END)
+    t13.update()
+    t13.insert(END, "Loading...")
+    t13.update()
+    info = os.popen(f'powershell.exe "Get-EventLog -LogName *"').read()
+    t13.delete("1.0",END)
+    t13.insert(1.0, info)
+
+def eventos_activos():
+    ocultar_paneles()
+    dia=str(entrada_de_dia.get())
+    evento=str(combo_eventos.get())
+    pw_eventos_activos.pack()
+    t14.delete("1.0",END)
+    t14.update()
+    t14.insert(END, "Loading...")
+    t14.update()
+    info = os.popen(f'powershell.exe "Get-EventLog -LogName {evento} -After (Get-Date).AddDays(-{dia}) | Select-Object TimeGenerated, Source, Message"').read()
+    t14.delete("1.0",END)
+    t14.insert(1.0, info)
+    entrada_de_dia.delete(0, END)
+
+
+def detener_servicio():
+    servicio=str(entrada_de_servicio.get())
+    os.system(f'powershell.exe "Stop-Service -Name {servicio}"')
+    entrada_de_servicio.delete(0, END)
+    servicios()
+
+def iniciar_servicio():
+    servicio=str(entrada_iniciar_Servicio.get())
+    os.system(f'powershell.exe "Start-Service -Name {servicio}"')
+    entrada_iniciar_Servicio.delete(0, END)
+    servicios()
+
 
 #------Active Directory--------#
 
@@ -128,7 +249,6 @@ def usuarios_de_Active_directory():
     info = os.popen(f'powershell.exe "Get-ADUser -Filter * | Select-Object -Property Name"').read()
     t6.delete("1.0",END)
     t6.insert(1.0, info)
-
 
 def grupos_de_Active_directory():
     ocultar_paneles()
@@ -182,19 +302,7 @@ def Eliminar_miembro_de_grupo():
     os.system(f'powershell.exe "Remove-ADGroupMember -Identity "{grupo}" -Members "{usuario}" -Confirm:$false"')
     entrada_de_grupo.delete(0, END)
     entrada_de_usuario.delete(0, END)
-    administrar() 
-
-def detener_servicio():
-    servicio=str(entrada_de_servicio.get())
-    os.system(f'powershell.exe "Stop-Service -Name {servicio}"')
-    entrada_de_servicio.delete(0, END)
-    servicios()
-
-def iniciar_servicio():
-    servicio=str(entrada_iniciar_Servicio.get())
-    os.system(f'powershell.exe "Start-Service -Name {servicio}"')
-    entrada_iniciar_Servicio.delete(0, END)
-    servicios()
+    administrar()
 
 def crear_grupo():
     grupo=str(entrada_crear_grupo.get())
@@ -218,6 +326,7 @@ def administrar():
     info = os.popen(f'powershell.exe "Get-ADGroup -Filter * | Select-Object -Property Name"').read()
     t10.delete("1.0",END)
     t10.insert(1.0, info)
+
 
 #------MAIN------#
 
@@ -289,6 +398,17 @@ boton.pack()
 t11 = scrolledtext.ScrolledText(pw_formatear, height=30, width=120)
 t11.pack(expand=True, fill='both')
 
+#---------FORMATEO REALIZADO-------------#
+
+pw_formateo_realizado = PanedWindow()
+pw_formateo_realizado.pack(expand=True, fill='both')
+pw_formateo_realizado.pack()
+pw_formateo_realizado.pack_forget()
+t12 = scrolledtext.ScrolledText(pw_formateo_realizado, height=30, width=120)
+t12.pack(expand=True, fill='both')
+boton_volver=Button(pw_formateo_realizado, text="VOLVER", command=formatear)
+boton_volver.pack()
+
 #--------SERVICIOS DEL SISTEMA------------#
 
 pw_servicios = PanedWindow()
@@ -306,7 +426,34 @@ boton_parar.pack()
 t9 = scrolledtext.ScrolledText(pw_servicios, height=30, width=120)
 t9.pack(expand=True, fill='both')
 
+#------PW_DE_EVENTOS------#
 
+get_events = get_event_type()
+pw_eventos = PanedWindow()
+pw_eventos.pack(expand=True, fill='both')
+pw_eventos.pack()
+pw_eventos.pack_forget()
+combo_eventos = ttk.Combobox(pw_eventos, values=get_events, width=20)
+combo_eventos.pack()
+label=Label(pw_eventos, text="Días del evento")
+label.pack()
+entrada_de_dia=ttk.Entry(pw_eventos)
+entrada_de_dia.pack()
+boton_de_visualizar_evento=Button(pw_eventos, text="VISUALIZAR EVENTOS", command=eventos_activos)
+boton_de_visualizar_evento.pack()
+t13 = scrolledtext.ScrolledText(pw_eventos, height=30, width=120)
+t13.pack(expand=True, fill='both')
+
+#----PANEL DE LOS EVENTOS ACTIVOS--------#
+
+pw_eventos_activos = PanedWindow()
+pw_eventos_activos.pack(expand=True, fill='both')
+pw_eventos_activos.pack()
+pw_eventos_activos.pack_forget()
+t14 = scrolledtext.ScrolledText(pw_eventos_activos, height=30, width=120)
+t14.pack(expand=True, fill='both')
+boton_volver=Button(pw_eventos_activos, text="VOLVER", command=eventlog)
+boton_volver.pack()
 
 #----USUARIOS DE ACTIVE DIRECTORY-----------#
 
@@ -327,6 +474,7 @@ t6.pack(expand=True, fill='both')
 
 #-----GRUPOS DE ACTIVE DIRECTORY--------#
 
+#---------CREACIÓN Y ELIMINACIÓN DE GRUPO--------#
 pw_grupos_active_directory = PanedWindow()
 pw_grupos_active_directory.pack(expand=True, fill='both')
 pw_grupos_active_directory.pack()
@@ -340,6 +488,7 @@ boton_borrar_grupos.pack()
 t7 = scrolledtext.ScrolledText(pw_grupos_active_directory, height=30, width=120)
 t7.pack(expand=True, fill='both')
 
+#-------------BUSCAR MIEMBROS Y AÑADIR USUARIOS-----------#
 pw_administrar_grupos=PanedWindow()
 pw_administrar_grupos.pack(expand=True, fill='both')
 pw_administrar_grupos.pack()
@@ -374,7 +523,7 @@ t8.pack(expand=True, fill='both')
 retorno=Button(pw_filtrado, text="VOLVER", command=administrar)
 retorno.pack()
 
-#------CONFIGURACIÓN DEL MENÚ MENU------#
+#------CONFIGURACIÓN DEL MENÚ------#
 
 barra_de_menu = Menu(raiz)
 raiz.config(menu=barra_de_menu)
@@ -400,6 +549,8 @@ barra_de_menu.add_cascade(label="ActiveDirectory", menu=Active_Directory)
 system_administration.add_command(label="Características generales del sistema", command=informacion_general_del_sistema)
 system_administration.add_command(label="Red del sistema", command=red)
 system_administration.add_command(label="Puertos del sistema", command=puertos_abiertos)
+system_administration.add_command(label="Eventos del sistema", command=eventlog)
+system_administration.add_command(label="Rendimiento", command=rendimiento)
 
 #-------SUBMENUS PARA LA ADMINISTRACIÓN DE LOS PROCESOS-------#
 
@@ -413,11 +564,10 @@ Disp.add_command(label="Formatear Volumen", command=formatear)
 
 Services.add_command(label="Visualizar servicios", command=servicios)
 
-#------SUBMENU DE ACTIVE DIRECRTORY------#
+#------SUBMENU DE ACTIVE DIRECTORY------#
 
 Active_Directory.add_command(label="Usuarios del Dominio", command=usuarios_de_Active_directory)
 Active_Directory.add_command(label="Grupos del Dominio", command=grupos_de_Active_directory)
 Active_Directory.add_command(label="Administrar miembros de un grupo", command=administrar)
-#-------SUBMENU PARA EL PENTESTING---------#
 
 raiz.mainloop()
